a4f378fac62044e789bba28119ca0aa85aa3d97f
jonah.sullivan <jonah.sullivan@ga.gov.au>
2019-01-26 17:44:31 +1100

    added minimal example

--- 02 widgets_example.py
+++ 02 widgets_example.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-
-
 #############################################################################
 ##
 ## Copyright (C) 2013 Riverbank Computing Limited.
--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QApplication, QDialog
 
-# import the dialog form that has been converted into python
+# import the dialog form ui file after conversion into python
 from hello_world_dialog import Ui_HelloWorldDialogBase
 
 if __name__ == '__main__':
@@ -12,18 +12,20 @@ if __name__ == '__main__':
     helloWorld = Ui_HelloWorldDialogBase()
     helloWorld.setupUi(window)
 
-    # populate a comboBox element with sequential values
+    # populate the comboBox element with sequential values
     for i in range(10):
         helloWorld.comboBox.addItem(str(i))
 
     # make the "Hello?" push button do something
-    def hello():
+
+    def hello():  # SLOT
+
         if helloWorld.messageBox.text() == "":
             helloWorld.messageBox.setText("Hello, World!")
         else:
             helloWorld.messageBox.setText("")
 
-    helloWorld.pushButton.clicked.connect(hello)
+    helloWorld.pushButton.clicked.connect(hello)  # SIGNAL
 
     window.show()
     sys.exit(app.exec_())
